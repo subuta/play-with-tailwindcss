ee6f73cbd105f9f914afc27420767f5c64032c06
subuta <shunta.3110@gmail.com>
2019-09-19 22:02:50 +0900

    Add initial files.

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default () => {
+  return (
+    <div className='h-screen w-screen p-4 bg-gray-300 flex items-center justify-center'>
+      <h1 className='text-6xl text-red-500'>Hello world</h1>
+    </div>
+  )
+}
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import '../tailwind.css'
+
+import App from './App'
+
+const app = (
+  <App />
+)
+
+ReactDOM.render(app, document.getElementById('app'))
